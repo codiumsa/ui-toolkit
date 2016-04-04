712ebb70c4830bf214cddd06313f0924a76586e7
Guillermo Peralta <gperaltascura@gmail.com>
2016-04-04 16:57:36 -0400

    Servicio para convertir a y desde unidad de medida base

--- dist/qualita-core-frontend.js
+++ dist/qualita-core-frontend.js
@@ -2609,3 +2609,39 @@ angular.module('qualitaCoreFrontend')
       }*/
     };
   });
+
+/**
+ * Created by codiumsa on 4/4/16.
+ */
+angular.module('qualitaCoreFrontend')
+  .factory('Util', Util);
+
+
+function Util() {
+  var service = {
+    toUnidadMedidaBase: toUnidadMedidaBase,
+    fromUnidadMedidaBase: fromUnidadMedidaBase
+  };
+
+  return service;
+
+  function toUnidadMedidaBase(cantidad, unidadMedida) {
+      var multiplicador=1;
+      var unidadActual = unidadMedida;
+      while(!unidadActual.esBase){
+        multiplicador=multiplicador*unidadActual.cantidad;
+        unidadActual=unidadActual.unidadContenida;
+      }
+      return cantidad * multiplicador;
+  }
+
+  function fromUnidadMedidaBase(cantidad, unidadObjetivo) {
+    var multiplicador = 1;
+    var unidadActual = unidadObjetivo;
+    while (!unidadActual.esBase) {
+      multiplicador = multiplicador * unidadActual.cantidad;
+      unidadActual = unidadActual.unidadContenida;
+    }
+    return cantidad / multiplicador;
+  }
+}
