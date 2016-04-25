210c66f544007140a2aa815a3822122a6f824a3c
SAUL SOTO <ssoto@tdn.com.py>
2016-04-25 12:50:54 -0400

    Se modifica estilo del tdn-data table
    Se agrega un efecto de mouse over sobre la grilla.
    Se condensa la grilla

--- dist/qualita-core-frontend.js
+++ dist/qualita-core-frontend.js
@@ -50,96 +50,6 @@
 
 'use strict';
 
-angular.module('qualitaCoreFrontend')
-  .controller('BasicController', ['$rootScope', '$scope', 'formFactory', '$location',
-    '$state', '$injector',
-    function($rootScope, $scope, formFactory, $location,
-      $state, $injector) {
-
-      $scope.activate = function() {
-        $scope.schema = $scope.factory.schema();
-        $scope.options = formFactory.defaultOptions();
-
-        if ($state.is($scope.newProperties.state)) {
-          activateNew();
-        } else if ($state.is($scope.editProperties.state)) {
-          activateEdit();
-        } else if ($state.is($scope.viewProperties.state)) {
-          activateView();
-        }
-        
-        $rootScope.isProcessing = false;
-      }
-
-      function activateNew() {
-        if (!formFactory.canCreate($scope.resources)) {
-          var notify = $injector.get('notify');
-          // error de autorización
-          notify({
-            message: "No tiene permiso de creación",
-            classes: ['alert-danger'],
-            position: 'right'
-          });
-          $location.path('/');
-        }
-        $scope.title = $scope.newProperties.title;
-        $scope.form = $scope.factory.form('new');
-        $scope.model = {};
-        $scope.schema.readonly = false;
-      }
-
-      function activateEdit() {
-        if (!formFactory.canEdit($scope.resources)) {
-          var notify = $injector.get('notify');
-          // error de autorización
-          notify({
-            message: "No tiene permiso de edición",
-            classes: ['alert-danger'],
-            position: 'right'
-          });
-          $location.path('/');
-        }
-        $scope.model = $scope.prepService;
-        $scope.entidadId = $scope.model.id;
-        $scope.entidad = $scope.editProperties.entidad;
-        $scope.form = $scope.factory.form('edit');
-        $scope.title = $scope.editProperties.title;
-        $scope.schema.readonly = false;
-      }
-
-      function activateView() {
-        if (!formFactory.canList($scope.resources)) {
-          var notify = $injector.get('notify');
-          // error de autorización
-          notify({
-            message: "No tiene permiso de vista",
-            classes: ['alert-danger'],
-            position: 'right'
-          });
-          $location.path('/');
-        }
-        $scope.options = formFactory.defaultViewOptions();
-        $scope.model = $scope.prepService;
-        $scope.entidadId = $scope.model.id;
-        $scope.entidad = $scope.viewProperties.entidad;
-        $scope.form = $scope.factory.form('view');
-        $scope.title = $scope.viewProperties.title;
-        $scope.view = true;
-        $scope.schema.readonly = true;
-      }
-
-      $scope.submit = function(form) {
-        formFactory.defaultSubmit($scope.resource, $scope, form, $scope.factory);
-      };
-
-      $scope.cancel = function() {
-        $location.path('/' + $scope.resource);
-      };
-
-    }
-  ]);
-'use strict';
-
 /**
  * @ngdoc directive
  * @name qualita.directive:fileupload
@@ -979,7 +889,7 @@ angular.module('qualitaCoreFrontend')
       '</div>' +
       '<div class="widget-body">' +
           '<div class="table-responsive">' +
-            '<table datatable="" dt-options="dtOptions" dt-columns="dtColumns" dt-instance="dtInstanceCallback" width=100% class="table table-striped no-footer">' +
+            '<table datatable="" dt-options="dtOptions" dt-columns="dtColumns" dt-instance="dtInstanceCallback" width=100% class="table table-hover table-responsive table-condensed no-footer">' +
             '</table>' +
           '</div>' +
           '<div ng-if="selected">' +
@@ -1766,6 +1676,96 @@ angular.module('qualitaCoreFrontend')
     };
   });
 
+'use strict';
+
+angular.module('qualitaCoreFrontend')
+  .controller('BasicController', ['$rootScope', '$scope', 'formFactory', '$location',
+    '$state', '$injector',
+    function($rootScope, $scope, formFactory, $location,
+      $state, $injector) {
+
+      $scope.activate = function() {
+        $scope.schema = $scope.factory.schema();
+        $scope.options = formFactory.defaultOptions();
+
+        if ($state.is($scope.newProperties.state)) {
+          activateNew();
+        } else if ($state.is($scope.editProperties.state)) {
+          activateEdit();
+        } else if ($state.is($scope.viewProperties.state)) {
+          activateView();
+        }
+        
+        $rootScope.isProcessing = false;
+      }
+
+      function activateNew() {
+        if (!formFactory.canCreate($scope.resources)) {
+          var notify = $injector.get('notify');
+          // error de autorización
+          notify({
+            message: "No tiene permiso de creación",
+            classes: ['alert-danger'],
+            position: 'right'
+          });
+          $location.path('/');
+        }
+        $scope.title = $scope.newProperties.title;
+        $scope.form = $scope.factory.form('new');
+        $scope.model = {};
+        $scope.schema.readonly = false;
+      }
+
+      function activateEdit() {
+        if (!formFactory.canEdit($scope.resources)) {
+          var notify = $injector.get('notify');
+          // error de autorización
+          notify({
+            message: "No tiene permiso de edición",
+            classes: ['alert-danger'],
+            position: 'right'
+          });
+          $location.path('/');
+        }
+        $scope.model = $scope.prepService;
+        $scope.entidadId = $scope.model.id;
+        $scope.entidad = $scope.editProperties.entidad;
+        $scope.form = $scope.factory.form('edit');
+        $scope.title = $scope.editProperties.title;
+        $scope.schema.readonly = false;
+      }
+
+      function activateView() {
+        if (!formFactory.canList($scope.resources)) {
+          var notify = $injector.get('notify');
+          // error de autorización
+          notify({
+            message: "No tiene permiso de vista",
+            classes: ['alert-danger'],
+            position: 'right'
+          });
+          $location.path('/');
+        }
+        $scope.options = formFactory.defaultViewOptions();
+        $scope.model = $scope.prepService;
+        $scope.entidadId = $scope.model.id;
+        $scope.entidad = $scope.viewProperties.entidad;
+        $scope.form = $scope.factory.form('view');
+        $scope.title = $scope.viewProperties.title;
+        $scope.view = true;
+        $scope.schema.readonly = true;
+      }
+
+      $scope.submit = function(form) {
+        formFactory.defaultSubmit($scope.resource, $scope, form, $scope.factory);
+      };
+
+      $scope.cancel = function() {
+        $location.path('/' + $scope.resource);
+      };
+
+    }
+  ]);
 'use strict';
 
 /**
