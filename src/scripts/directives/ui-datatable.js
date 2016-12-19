5c2fd94545544497cbdc2d436e7a327aca3fa86e
Jorge Ramírez <jorgeramirez1990@gmail.com>
2016-12-18 22:18:30 -0300

    Temporalmente se mandan los filtros en el query string
    Actualizar eventualmente el parser del backend para que reciba los
    filtros en el body del POST
    
    refs #DIIBSER-28

--- src/scripts/directives/ui-datatable.js
+++ src/scripts/directives/ui-datatable.js
@@ -84,14 +84,14 @@ angular.module('ui')
           }
           data.rangeSeparator = rangeSeparator;
 
-          var xhr = $resource(urlTemplate($scope.options), {}, {
+          var xhr = $resource(urlTemplate($scope.options) + $.param(data), {}, {
             query: {
               isArray: false,
               method: 'POST'
             }
           });
 
-          xhr.query(data).$promise.then(function(response) {
+          xhr.query({}).$promise.then(function(response) {
             var datos = response.data;
             if(datos) {
               datos.forEach(function(registro) {
