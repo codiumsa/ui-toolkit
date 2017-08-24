ea08617c51aeec22392d0a304dab0629b572f76a
Nahuel Hernández <jnahuelhernandez@gmail.com>
2017-08-24 08:59:21 -0400

    Build 0.4.2
    - Fix en presentacion fileupload. Fix de resumen de subida

--- dist/ui.js
+++ dist/ui.js
@@ -72,6 +72,92 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
 (function () {
   'use strict';
 
+  angular.module('ui').controller('BasicController', ['$rootScope', '$scope', 'formFactory', '$location', '$state', '$injector', function ($rootScope, $scope, formFactory, $location, $state, $injector) {
+
+    $scope.activate = function () {
+      $scope.schema = $scope.factory.schema();
+      $scope.options = formFactory.defaultOptions();
+
+      if ($state.is($scope.newProperties.state)) {
+        activateNew();
+      } else if ($state.is($scope.editProperties.state)) {
+        activateEdit();
+      } else if ($state.is($scope.viewProperties.state)) {
+        activateView();
+      }
+
+      $rootScope.isProcessing = false;
+    };
+
+    function activateNew() {
+      if (!formFactory.canCreate($scope.resources)) {
+        var notify = $injector.get('notify');
+        // error de autorización
+        notify({
+          message: 'No tiene permiso de creación',
+          classes: ['alert-danger'],
+          position: 'right'
+        });
+        $location.path('/');
+      }
+      $scope.title = $scope.newProperties.title;
+      $scope.form = $scope.factory.form('new');
+      $scope.model = {};
+      $scope.schema.readonly = false;
+    }
+
+    function activateEdit() {
+      if (!formFactory.canEdit($scope.resources)) {
+        var notify = $injector.get('notify');
+        // error de autorización
+        notify({
+          message: 'No tiene permiso de edición',
+          classes: ['alert-danger'],
+          position: 'right'
+        });
+        $location.path('/');
+      }
+      $scope.model = $scope.prepService;
+      $scope.entidadId = $scope.model.id;
+      $scope.entidad = $scope.editProperties.entidad;
+      $scope.form = $scope.factory.form('edit');
+      $scope.title = $scope.editProperties.title;
+      $scope.schema.readonly = false;
+    }
+
+    function activateView() {
+      if (!formFactory.canList($scope.resources)) {
+        var notify = $injector.get('notify');
+        // error de autorización
+        notify({
+          message: 'No tiene permiso de vista',
+          classes: ['alert-danger'],
+          position: 'right'
+        });
+        $location.path('/');
+      }
+      $scope.options = formFactory.defaultViewOptions();
+      $scope.model = $scope.prepService;
+      $scope.entidadId = $scope.model.id;
+      $scope.entidad = $scope.viewProperties.entidad;
+      $scope.form = $scope.factory.form('view');
+      $scope.title = $scope.viewProperties.title;
+      $scope.view = true;
+      $scope.schema.readonly = true;
+    }
+
+    $scope.submit = function (form) {
+      formFactory.defaultSubmit($scope.resource, $scope, form, $scope.factory);
+    };
+
+    $scope.cancel = function () {
+      $location.path('/' + $scope.resource);
+    };
+  }]);
+})();
+(function () {
+  'use strict';
+
   angular.module('ui').directive('aDisabled', function () {
     return {
       compile: function compile(tElement, tAttrs, transclude) {
@@ -2632,92 +2718,6 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
       $state.go(dest);
     };
   }
-})();
-(function () {
-  'use strict';
-
-  angular.module('ui').controller('BasicController', ['$rootScope', '$scope', 'formFactory', '$location', '$state', '$injector', function ($rootScope, $scope, formFactory, $location, $state, $injector) {
-
-    $scope.activate = function () {
-      $scope.schema = $scope.factory.schema();
-      $scope.options = formFactory.defaultOptions();
-
-      if ($state.is($scope.newProperties.state)) {
-        activateNew();
-      } else if ($state.is($scope.editProperties.state)) {
-        activateEdit();
-      } else if ($state.is($scope.viewProperties.state)) {
-        activateView();
-      }
-
-      $rootScope.isProcessing = false;
-    };
-
-    function activateNew() {
-      if (!formFactory.canCreate($scope.resources)) {
-        var notify = $injector.get('notify');
-        // error de autorización
-        notify({
-          message: 'No tiene permiso de creación',
-          classes: ['alert-danger'],
-          position: 'right'
-        });
-        $location.path('/');
-      }
-      $scope.title = $scope.newProperties.title;
-      $scope.form = $scope.factory.form('new');
-      $scope.model = {};
-      $scope.schema.readonly = false;
-    }
-
-    function activateEdit() {
-      if (!formFactory.canEdit($scope.resources)) {
-        var notify = $injector.get('notify');
-        // error de autorización
-        notify({
-          message: 'No tiene permiso de edición',
-          classes: ['alert-danger'],
-          position: 'right'
-        });
-        $location.path('/');
-      }
-      $scope.model = $scope.prepService;
-      $scope.entidadId = $scope.model.id;
-      $scope.entidad = $scope.editProperties.entidad;
-      $scope.form = $scope.factory.form('edit');
-      $scope.title = $scope.editProperties.title;
-      $scope.schema.readonly = false;
-    }
-
-    function activateView() {
-      if (!formFactory.canList($scope.resources)) {
-        var notify = $injector.get('notify');
-        // error de autorización
-        notify({
-          message: 'No tiene permiso de vista',
-          classes: ['alert-danger'],
-          position: 'right'
-        });
-        $location.path('/');
-      }
-      $scope.options = formFactory.defaultViewOptions();
-      $scope.model = $scope.prepService;
-      $scope.entidadId = $scope.model.id;
-      $scope.entidad = $scope.viewProperties.entidad;
-      $scope.form = $scope.factory.form('view');
-      $scope.title = $scope.viewProperties.title;
-      $scope.view = true;
-      $scope.schema.readonly = true;
-    }
-
-    $scope.submit = function (form) {
-      formFactory.defaultSubmit($scope.resource, $scope, form, $scope.factory);
-    };
-
-    $scope.cancel = function () {
-      $location.path('/' + $scope.resource);
-    };
-  }]);
 })();
 (function () {
   'use strict';
